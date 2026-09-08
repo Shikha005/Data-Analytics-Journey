cbde355fe936cb87acd5ae29c80ff7eebf378290
Shikha005 <shikhagour2005@gmail.com>
2026-09-08 23:34:43 +0530

    Add files via upload

---
+++ SQL/Day 63-sql c.sql
@@ -0,0 +1,15 @@
+﻿--Delete all orders whose status is Cancelled and whose amount is less than ₹1,000.
+
+DELETE FROM order__
+WHERE Status = 'Cancelled'
+AND Amount < 1000;
+
+--Reduce the stock of each product by 1 for products that have at least one order.
+
+UPDATE p
+SET p.Stock = p.Stock - 1
+FROM product__ p
+WHERE p.ProductID IN (
+    SELECT DISTINCT ProductID
+    FROM order__
+);
