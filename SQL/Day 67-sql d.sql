0cca8b6cf90a34acbac605c4df9d9c0ff4eec592
Shikha005 <shikhagour2005@gmail.com>
2026-09-13 23:30:51 +0530

    Add files via upload

---
+++ SQL/Day 67-sql d.sql
@@ -0,0 +1,12 @@
+﻿--Display Customer Name and Product Name for products purchased by each customer. Show only products where the product price is greater than ₹5,000.
+SELECT DISTINCT
+    c.CustomerName,
+    p.ProductName,
+    p.ProductPrice
+FROM customer__ c
+INNER JOIN order__ o
+    ON c.CustomerID = o.CustomerID
+INNER JOIN product__ p
+    ON o.ProductID = p.ProductID
+WHERE p.ProductPrice > 5000
+ORDER BY p.ProductPrice DESC;
