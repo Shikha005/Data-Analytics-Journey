3a577ed0c5bb33b236b52566018faa206bc57816
Shikha005 <shikhagour2005@gmail.com>
2026-09-19 00:07:57 +0530

    Add files via upload

---
+++ SQL/Day 72-sql a.sql
@@ -0,0 +1,17 @@
+﻿--Using a CTE, find orders whose order value is greater than ₹50,000.
+
+WITH OrderDetails AS
+(
+SELECT OrderID,CustomerID,ProductID,Quantity,UnitPrice,Quantity * UnitPrice AS OrderValue
+FROM order__
+)
+SELECT OrderID,CustomerID,ProductID,OrderValue
+FROM OrderDetails
+WHERE OrderValue > 50000
+ORDER BY OrderValue DESC;
+
+--Find employees who do not have a manager assigned and display "No Manager" instead of NULL.
+
+SELECT EmployeeID,EmployeeName,Department,COALESCE(
+CAST(ManagerID AS VARCHAR(10)),'No Manager') AS Manager
+FROM employee__;
