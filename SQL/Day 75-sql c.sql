fe995ef2f64848f6ccb0b931e75b781498563a4c
Shikha005 <shikhagour2005@gmail.com>
2026-09-23 22:31:23 +0530

    Add files via upload

---
+++ SQL/Day 75-sql c.sql
@@ -0,0 +1,21 @@
+﻿--Find customers whose total spending is greater than ₹50,000.
+
+SELECT c.CustomerID,c.CustomerName,SUM(o.Quantity * o.UnitPrice) AS TotalSpending
+FROM customer__ c
+INNER JOIN order__ o
+ON c.CustomerID = o.CustomerID
+GROUP BY c.CustomerID, c.CustomerName
+HAVING SUM(o.Quantity * o.UnitPrice) > 50000
+ORDER BY TotalSpending DESC;
+
+--Find the latest order placed by each customer.
+
+WITH LatestOrders AS
+(
+SELECT CustomerID,OrderID,OrderDate,Quantity,
+ROW_NUMBER() OVER (PARTITION BY CustomerID ORDER BY OrderDate DESC, OrderID DESC) AS rn
+FROM order__
+)
+SELECT CustomerID,OrderID,OrderDate,Quantity
+FROM LatestOrders
+WHERE rn = 1;
